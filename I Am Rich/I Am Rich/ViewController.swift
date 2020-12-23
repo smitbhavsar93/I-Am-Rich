b317106d097d15b363553bb100ae28f02cc6f9d4
Smit Bhavsar <smit.bhavsar93@gmail.com>
2020-12-23 15:36:52 -0500

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Smit Bhavsar on SundayDecember202020.
+//  Copyright © 2020 Smit Bhavsar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
